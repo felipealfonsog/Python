07c20923e79e8d137d1d4cb6853ca05b25e9c027
Felipe <f.alfonso@res-ear.ch>
2018-08-09 03:07:45 -0400

    changes and getting done

--- First.py
+++ First.py
@@ -1,2 +1,32 @@
-printf("Hello World")
+# printf("Hello World")
 
+class MITxStaff(object):
+    firstname = ""
+    lastname = ""
+
+
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+
+   class TA(MITxStaff):
+       awesomeLevel = 0
+
+       def __init__(self, firstname, lastname, awesomeLevel):
+           self.awesomeLevel = awesomeLevel
+           MITxStaff.__init__(self, firstname, lastname)
+
+
+       def toString(self):
+           return "{} {} has an awesome level of {}".format(self.firstname, self.lastname, self.awesomeLevel)
+
+
+
+
+
+    Nitish = TA("Nitish", "Mital", 100)
+    Jing = TA("Jing", "Ma", 9001)
+
+    print(Nitish.toString())
+    print(Jing.toString())
