adfc71a9255dd4349366c3338a058633ea460777
Felipe <f.alfonso@res-ear.ch>
2018-08-10 03:39:47 -0400

    changes and getting done

--- bigO.py
+++ bigO.py
@@ -1,10 +1,25 @@
 #big o notation
 #O(1)
 
-num = 10
+num = 100
 
 def deductOne(num):
     num -=1
     return num
 
 print "O(1):", "Win", deductOne(num), "Win"
+
+#logaritmic O O(log n)
+
+num = 10
+
+def divide(num):
+    while num > 1:
+        num /= 2
+        print(num)
+    return num
+
+print "O(log n):"
+divide(num)
+print
+
