00d7c79aef4ca66414656c4374918424cc071b74
Felipe <f.alfonso@res-ear.ch>
2018-08-11 04:35:29 -0400

    77r7t7t7

--- bigO.py
+++ bigO.py
@@ -23,3 +23,29 @@ print "O(log n):"
 divide(num)
 print
 
+#O(n)
+
+
+num = 10
+
+def addOnesToTestList(num):
+    testList = []
+    for i in range(0, num):
+        testList.append(1)
+        print(testList)
+    return testList
+
+print "(O(n): )"
+addOnesToTestList
+print
+
+
+#O(n loh n)
+
+testList = [1, 43, 31, 21, 6, 96, 48, 13, 25, 5]
+
+def mergeSort(testList):
+    if len(testList) < 2:
+        return testList
+    middle = len(testList) / 2:
+    left(mergeSort(testList[:middle]))
