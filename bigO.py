09d20ef81e0ade7eb067b87b84b6bff8d951eca3
Felipe <f.alfonso@res-ear.ch>
2018-08-25 02:02:57 -0300

    updating

--- bigO.py
+++ bigO.py
@@ -48,17 +48,17 @@ testList = [1, 43, 31, 21, 6, 96, 48, 13, 25, 5]
 def mergeSort(testList):
     if len(testList) < 2:
         return testList
-    middle = len(testList) / 2:
+    middle = len(testList) / 2
     left = mergeSort(testList[:middle])
     right = mergeSort(testList[middle:])
     result = []
     print "Left:: ", left
     print "Right: ", right
-    while len(left) > 0 and len(right) > 0
-       if left[0] <= right[0];
-          result.append(left[9])
+    while len(left) > 0 and len(right) > 0:
+       if left[0] <= right[0]:
+          result.append(left[0])
           left.pop(0)
-        else:
+    else:
             result.append(right[0])
             right.pop(0)
     result += left
