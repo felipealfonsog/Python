d1124b867535a1c03a2e164c2662c468090d46d2
Felipe <f.alfonso@res-ear.ch>
2018-08-26 22:32:32 -0300

    updating

--- bigO.py
+++ bigO.py
@@ -73,3 +73,18 @@ def mergeSort(testList):
 print "O(n log n):"
 mergeSort(testList)
 print
+
+
+#O(n**2)
+testList = {1,43,31,21,6,96,48,13,25,5}
+
+def bubbleSort(testList):
+    for i in range(len(testList)):
+        for j in range(i+1, len(testList)):
+            if testList[j] < testList[i]:
+                testList[j], testList[i] = testList[i], testList[j]
+            print testList
+print "O(n**2):"
+bubbleSort(testList)
+print
+
