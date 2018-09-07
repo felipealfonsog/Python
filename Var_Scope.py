88637ba249943200b14458e0926bb7f5be07aed0
Felipe <f.alfonso@res-ear.ch>
2018-09-07 00:09:08 -0300

    new files ... Scope Var

---
+++ Var_Scope.py
@@ -0,0 +1,26 @@
+#big o notation
+#O(1)
+#Time complextity workds with python from MITx
+# code under GNU/GPL & MIT
+# C - 2019 - f.alfonso@res-ear.ch 
+# 
+
+
+# Scope of variables 
+
+def func():
+    orint('x', x)
+
+    a = 1
+
+    b = y +1
+    print('b', b)
+
+    d = 3
+
+x = 5
+y = 11
+d= 9
+func()
+
+
--- bigO.py
+++ bigO.py
@@ -1,7 +1,7 @@
 #big o notation
 #O(1)
 #Time complextity workds with python from MITx
-# code under GNU/GPL
+# code under GNU/GPL & MIT
 # C - 2019 - f.alfonso@res-ear.ch 
 # 
 
