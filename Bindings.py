5c3f792133a4676446322c7479b2100e6775e7b9
Felipe <f.alfonso@res-ear.ch>
2018-09-14 01:40:19 -0300

    adding more stuff

---
+++ Bindings.py
@@ -0,0 +1,19 @@
+#big o notation
+#O(1)
+#Time complextity workds with python from MITx
+# code under GNU/GPL & MIT
+# C - 2019 - f.alfonso@res-ear.ch 
+# 
+
+
+# Bindings
+
+usa_gold = 46
+uk_gold = 27
+romania_gold = 1
+
+total_gold = usa_gold + uk_gold + romania_gold
+print(total_gold)
+
+romania_gold +=1
+print(total_gold)
