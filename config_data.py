f6863bdef823eacb2110500f359671f814385a00
Felipe Alfonso Gonzalez <felipe.dev.engr.js@Felipes-MacBook-Air.local>
2018-10-23 12:16:09 -0300

    updates & new files

--- config_data.py
+++ config_data.py
@@ -13,3 +13,23 @@ for datum in data_set:
 for thing in collection:
     process(thing)
 
+#----
+
+def make_collection(data_set):
+    collection = gte_initial_state();
+    state_var = None
+    for datum in data_set:
+        state_var = calculate_from(datum, state_var)
+        new = modify(datum, state_var)
+
+        collection.add_to(new)
+
+    else:
+        new = modify_differently(datum)
+        collection.add_to(new)
+
+return collection
+
+#Now acutally work with the data
+for thing in make_collection(data_set):
+    process(thing)
