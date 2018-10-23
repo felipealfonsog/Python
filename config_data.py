a47ada8875be6cdb8be112432f81904d6064be85
Felipe Alfonso Gonzalez <felipe.dev.engr.js@Felipes-MacBook-Air.local>
2018-10-23 12:01:27 -0300

    updates & new files

---
+++ config_data.py
@@ -0,0 +1,15 @@
+#configure the data to start with 
+collection= gte_initial_state()
+state_var = None
+for datum in data_set:
+    if condition(state_var):
+        state_var = calculate_from(datum)
+        new = modify(datum, state_var)
+        collection.add_to(new)
+    else:
+        new = modify_differently(datum)
+        collection.add_to(new)
+#Now actually work with the data
+for thing in collection:
+    process(thing)
+
