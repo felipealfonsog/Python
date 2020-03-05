141a2977edde08a8ff46ab882c83d934050a3213
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-03-05 02:12:42 -0300

    duplicate algo data estruc

--- First2.2.py
+++ First2.2.py
@@ -9,3 +9,13 @@ class MITxStaff(object):
     firstname = ""
     lastname = ""
 
+
+
+
+
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+
+def __init__(self, )
