cae9100df6cae93e1834505239b801618c32f721
Felipe Alfonso González <f.alfonso.go@gmail.com>
2020-07-08 20:28:44 -0400

    modf

--- eveOdd.py
+++ eveOdd.py
@@ -1,28 +1,40 @@
-#big o notation
-#O(1)
-#Time complextity workds with python from MITx
-# code under GNU/GPL & MIT
-# C - 2019 - f.alfonso@res-ear.ch 
+
+# code under MIT
+# C - 2019 - Felipe : f.alfonso@res-ear.ch 
 # 
 
-# x = int(input('Enter an int: '))
+
+# x = int(input('Enter an integer: '))
 # if x%2 == 0:
-#     print('')
+#     print(``)
 #     print('Even')
 # else:
-#     print('')
+#     print(``)
 #     print('Odd')
 # print('Done with conditional')
+#
+
+#
+# Video: Input/Output
+# Finger Exercises due Aug 5, 2020 19:30 -04
+#
+#
 
 
-# ----------
+# x = 1
+#
+# x_str = str(x)
+#
+# print("my fav num is" + x_str + "." + "x = " + x_str)
+#
+# text = input ("type somthing")
+#
+# '"foo"'
+#
+# print(5*text)
 
-x = int(input('Enter an integer: '))
-if x%2 == 0:
-    print(``)
-    print('Even')
-else:
-    print(``)
-    print('Odd')
-print('Done with conditional')
+
+if 6 > 7:
+   print("Yep")
+
 
